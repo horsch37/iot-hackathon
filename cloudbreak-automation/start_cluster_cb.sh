8e212692bc1ae87bfc4482bb29eb54ece20169de
Ashish Narasimham <ashish.narasimham@gmail.com>
2018-12-12 16:05:26 -0600

    updates to cb scripts

--- cloudbreak-automation/start_cluster_cb.sh
+++ cloudbreak-automation/start_cluster_cb.sh
@@ -1,1 +1,1 @@
-cb cluster create --cli-input-json cb_template.json --name hackathon-cluster --server https://192.168.56.100 --username anarasimham@hortonworks.com --password hortonworks
+cb cluster create --cli-input-json cb_template.json --name hackathon-cluster3 --server https://192.168.56.100 --username anarasimham@hortonworks.com --password hortonworks

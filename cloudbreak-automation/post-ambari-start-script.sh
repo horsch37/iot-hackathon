a6cafa947872c639670ecc9ceb55dd03ebc86a10
Ashish Narasimham <ashish.narasimham@gmail.com>
2018-12-12 15:46:17 -0600

    updated to remove sudo, all commands run as root

--- cloudbreak-automation/post-ambari-start-script.sh
+++ cloudbreak-automation/post-ambari-start-script.sh
@@ -2,32 +2,32 @@
 
 # Globals
 
-sudo . /opt/demo/shared.sh
+source /opt/demo/shared.sh
 
 do_prep() {
 
 
 # Install some packages
-sudo yum install -y git sendmail mailx
-sudo systemctl enable sendmail
+yum install -y git sendmail mailx
+systemctl enable sendmail
 
 }
 
 do_setup() {
 
-sudo ./setup.sh 
+./setup.sh 
 
 
 }
 
 do_start() {
 
-	sudo ./startsubmit.sh 
-	sudo ./submitdata.sh 
+	./startsubmit.sh 
+	./submitdata.sh 
 }
 
 #Execute functions
-sudo cd $DEMO_ROOT
+cd $DEMO_ROOT
 do_prep
 do_setup
 do_start
--- cloudbreak-automation/pre-ambari-start-script.sh
+++ cloudbreak-automation/pre-ambari-start-script.sh
@@ -2,24 +2,24 @@
 
 
 # Install some packages
-sudo yum -y install postgresql-server postgresql-contrib git
+yum -y install postgresql-server postgresql-contrib git
 
 #Clone Repo
-sudo git clone https://github.com/horsch37/iot-hackathon /opt/demo
+git clone https://github.com/horsch37/iot-hackathon /opt/demo
 
 # Source some shared variables
-sudo . /opt/demo/shared.sh
+source /opt/demo/shared.sh
 
 # Setup hostname
 
-sudo sed -i "s/localhost4.localdomain4/localhost4.localdomain4 $HOSTNAME/g" /etc/hosts
-sudo hostnamectl set-hostname $HOSTNAME
+sed -i "s/localhost4.localdomain4/localhost4.localdomain4 $HOSTNAME/g" /etc/hosts
+hostnamectl set-hostname $HOSTNAME
 
 # Install some packages
 
-sudo postgresql-setup initdb
-sudo systemctl start postgresql
-sudo systemctl enable postgresql
+postgresql-setup initdb
+systemctl start postgresql
+systemctl enable postgresql
 
 # Setup Postgres Users
 
@@ -33,8 +33,7 @@ sudo -u postgres psql -c "alter user hive with password 'hive'";
 sudo -u postgres psql -c "create database hive owner hive;"
 sudo -u postgres psql -c "grant all privileges on database hive to hive";
 
-sudo sed -i "s/\(127.0.0.1\/32\s\+\)ident/\1trust/g" /var/lib/pgsql/data/pg_hba.conf 
-sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /var/lib/pgsql/data/postgresql.conf
-sudo echo 'host    all          all            0.0.0.0/0  trust' | sudo tee -a /var/lib/pgsql/data/pg_hba.conf
-sudo systemctl restart postgresql
-
+sed -i "s/\(127.0.0.1\/32\s\+\)ident/\1trust/g" /var/lib/pgsql/data/pg_hba.conf 
+sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /var/lib/pgsql/data/postgresql.conf
+echo 'host    all          all            0.0.0.0/0  trust' | sudo tee -a /var/lib/pgsql/data/pg_hba.conf
+systemctl restart postgresql

b9468e3c8b28574d355f6dbc8401a280a7f344ab
Ashish Narasimham <ashish.narasimham@gmail.com>
2018-12-12 16:21:45 -0600

    changing superset db to postgres, changing general password to something that makes sense

--- cloudbreak-automation/start_cluster_cb.sh
+++ cloudbreak-automation/start_cluster_cb.sh
@@ -1,1 +1,1 @@
-cb cluster create --cli-input-json cb_template.json --name hackathon-cluster3 --server https://192.168.56.100 --username anarasimham@hortonworks.com --password hortonworks
+cb cluster create --cli-input-json cb_template.json --name hackathon-cluster4 --server https://192.168.56.100 --username anarasimham@hortonworks.com --password hortonworks

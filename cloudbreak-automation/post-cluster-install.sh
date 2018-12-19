a98f0111b3ee1e6df62ed1e5206623ac3eb46db2
horsch37 <jhorsch@hortonworks.com>
2018-12-19 11:07:23 -0500

    Update post-cluster-install.sh

--- cloudbreak-automation/post-cluster-install.sh
+++ cloudbreak-automation/post-cluster-install.sh
@@ -5,7 +5,7 @@
 source /opt/demo/shared.sh
 CNT=$(grep $HOSTNAME /etc/hosts | wc -l)
 if [ $CNT -eq 0 ] ; then
-   echo " $HOSTNAME" >> /etc/hosts
+   sed -i '/internal/ s/$/ '"$HOSTNAME"'/' /etc/hosts
    hostnamectl set-hostname $HOSTNAME
 fi
 

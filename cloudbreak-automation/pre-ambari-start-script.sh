acff7dc1ddacf832da1d3536eb4a10c90494e6b7
horsch37 <jhorsch@hortonworks.com>
2019-06-03 19:29:57 -0400

    Update pre-ambari-start-script.sh

--- cloudbreak-automation/pre-ambari-start-script.sh
+++ cloudbreak-automation/pre-ambari-start-script.sh
@@ -47,6 +47,11 @@ sudo -u postgres psql -c "alter user superset with password 'superset';"
 sudo -u postgres psql -c "create database superset owner superset;"
 sudo -u postgres psql -c "grant all privileges on database superset to superset;"
 
+sudo -u postgres createuser efm
+sudo -u postgres psql -c "alter user efm with password 'cloudera';"
+sudo -u postgres psql -c "create database efm owner efm;"
+sudo -u postgres psql -c "grant all privileges on database efm to efm;"
+
 sed -i "s/\(127.0.0.1\/32\s\+\)ident/\1trust/g" /var/lib/pgsql/data/pg_hba.conf 
 sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /var/lib/pgsql/data/postgresql.conf
 echo 'host    all          all            0.0.0.0/0  trust' | sudo tee -a /var/lib/pgsql/data/pg_hba.conf

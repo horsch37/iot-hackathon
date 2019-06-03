0e0c5d38f21b48ee7b4fdda6d94683995198e3d5
horsch37 <jhorsch@hortonworks.com>
2019-06-03 19:11:44 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -65,6 +65,29 @@ fi
 #start nifi
 #curl -u ${SUSR}:${SPWD} -H "X-Requested-By:ambari" -i -X PUT -d '{"RequestInfo": {"context" :"Start NIFI"}, "Body": {"ServiceInfo": {"state": "STARTED"}}}' http://demo.hortonworks.com:8080/api/v1/clusters/${CLUST}/services/NIFI
 
+#Install CEM Tarballs
+mkdir -p /opt/cloudera/cem
+wget https://archive.cloudera.com/CEM/centos7/1.x/updates/1.0.0.0/CEM-1.0.0.0-centos7-tars-tarball.tar.gz -P /opt/cloudera/cem
+tar xzf /opt/cloudera/cem/CEM-1.0.0.0-centos7-tars-tarball.tar.gz -C /opt/cloudera/cem
+tar xzf /opt/cloudera/cem/CEM/centos7/1.0.0.0-54/tars/efm/efm-1.0.0.1.0.0.0-54-bin.tar.gz -C /opt/cloudera/cem
+tar xzf /opt/cloudera/cem/CEM/centos7/1.0.0.0-54/tars/minifi/minifi-0.6.0.1.0.0.0-54-bin.tar.gz -C /opt/cloudera/cem
+tar xzf /opt/cloudera/cem/CEM/centos7/1.0.0.0-54/tars/minifi/minifi-toolkit-0.6.0.1.0.0.0-54-bin.tar.gz -C /opt/cloudera/cem
+/bin/rm -f /opt/cloudera/cem/CEM-1.0.0.0-centos7-tars-tarball.tar.gz
+ln -s /opt/cloudera/cem/efm-1.0.0.1.0.0.0-54 /opt/cloudera/cem/efm
+ln -s /opt/cloudera/cem/minifi-0.6.0.1.0.0.0-54 /opt/cloudera/cem/minifi
+ln -s /opt/cloudera/cem/efm/bin/efm.sh /etc/init.d/efm
+
+chown -R root:root /opt/cloudera/cem/efm-1.0.0.1.0.0.0-54
+chown -R root:root /opt/cloudera/cem/minifi-0.6.0.1.0.0.0-54
+chown -R root:root /opt/cloudera/cem/minifi-toolkit-0.6.0.1.0.0.0-54
+/bin/rm -f /opt/cloudera/cem/efm/conf/efm.properties
+cp /opt/demo/efm.properties /opt/cloudera/cem/efm/conf
+/bin/rm -f /opt/cloudera/cem/minifi/conf/bootstrap.conf
+cp /opt/demo/bootstrap.conf /opt/cloudera/cem/minifi/conf
+sed -i "s/YourHostname/`hostname -f`/g" /opt/cloudera/cem/efm/conf/efm.properties
+sed -i "s/YourHostname/`hostname -f`/g" /opt/cloudera/cem/minifi/conf/bootstrap.conf
+/opt/cloudera/cem/minifi/bin/minifi.sh install
+
 #Import Zeppelin
 export FNAME=/tmp/$$.cookies.txt
 export DATA="userName=${SUSR}&password=${SPWD}"

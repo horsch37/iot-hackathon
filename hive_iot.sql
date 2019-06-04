7d78ad820d978f26d41d55f2c811df010bac79b6
horsch37 <jhorsch@hortonworks.com>
2019-06-04 17:13:06 -0400

    Update hive_iot.sql

--- hive_iot.sql
+++ hive_iot.sql
@@ -1,4 +1,4 @@
-CREATE external TABLE hive_iot(`__time` string,id string, v double)
+CREATE external TABLE hive_iot(`__time` string,id string, v double, agent string)
 STORED AS ORC
 location '/warehouse/tablespace/managed/hive/hive_iot';
 
